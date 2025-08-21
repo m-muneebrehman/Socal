7418229529ffb7c9f889bc67136a86c6c7ccc949
m-muneebrehman <161295843+m-muneebrehman@users.noreply.github.com>
2025-08-21 14:11:14 +0500

    Update ConsultationForm.tsx
    updated reza email

--- src/components/common/ConsultationForm.tsx
+++ src/components/common/ConsultationForm.tsx
@@ -208,7 +208,7 @@ const ConsultationForm = ({
 											<path d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 											<polyline points="22,6 12,13 2,6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 										</svg>
-										<span>reza@primelocalhomes.com</span>
+										<span>reza@socalprimehomes.com</span>
 									</div>
 									{/* Address */}
 									<div className="contact-item">
